b45b587973525daf5a32f6ea14817af44fb83dc6
Martin <maritn.muriuki@strathmore.edu>
2022-02-21 13:11:09 +0300

    Create user screens

--- lib/models/place.dart
+++ lib/models/place.dart
@@ -8,8 +8,8 @@ class PlaceLocation {
 
   PlaceLocation(
     this.address, {
-    required this.latitude,
-    required this.longitude,
+    @required this.latitude,
+    @required this.longitude,
   });
 }
 
@@ -20,9 +20,9 @@ class Place {
   final File image;
 
   Place({
-    required this.id,
-    required this.title,
-    required this.location,
-    required this.image,
+    @required this.id,
+    @required this.title,
+    @required this.location,
+    @required this.image,
   });
 }
