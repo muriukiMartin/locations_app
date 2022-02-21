2da593904a3e0b3218adeba4e29c2200f937b794
Martin <maritn.muriuki@strathmore.edu>
2022-02-21 17:33:03 +0300

    Create place details form

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import './providers/great_places.dart';
 import './screens/places_list_screen.dart';
+import './screens/add_place_screen.dart';
 
 void main() => runApp(MyApp());
 
@@ -17,6 +18,9 @@ class MyApp extends StatelessWidget {
           accentColor: Colors.amber,
         ),
         home: PlacesListScreen(),
+        routes: {
+          AddPlaceScreen.routeName:(context) => AddPlaceScreen(),
+        },
         debugShowCheckedModeBanner: false,
       ),
     );
